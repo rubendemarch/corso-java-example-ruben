f0e45bd551982bddb746e63c2314a57bc2732611
rdm.15@alice.it <rdm.15@alice.it@203effc9-9d0e-70a1-7f8a-e44a82e94dd5>
2012-11-12 13:51:11 +0000

    Aggiunte Exception e finito wrapper per Properties

---
+++ trunk/test1/src/exception/ConfigFileNotValid.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package exception;
+
+/**
+ * @author ALFA403
+ *
+ */
+public class ConfigFileNotValid extends ConfigFileNotFound {
+
+	/**
+	 * 
+	 */
+	public ConfigFileNotValid() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public ConfigFileNotValid(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ConfigFileNotValid(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ConfigFileNotValid(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public ConfigFileNotValid(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
