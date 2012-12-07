641a274966ddedd9537008ff927c64fea13983a0
rdm.15@alice.it <rdm.15@alice.it@203effc9-9d0e-70a1-7f8a-e44a82e94dd5>
2012-12-07 11:03:32 +0000

    pagina gestione lingue nuda

--- trunk/e-commerce/src/it/ecommerce/servlet/common/language/ManageLanguages.java
+++ trunk/e-commerce/src/it/ecommerce/servlet/common/language/ManageLanguages.java
@@ -2,8 +2,10 @@ package it.ecommerce.servlet.common.language;
 
 import it.ecommerce.servlet.RootServlet;
 import it.ecommerce.util.constants.Common;
+import it.ecommerce.util.constants.Request;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -50,19 +52,49 @@ public class ManageLanguages extends RootServlet {
 
 	protected void process(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		loadLanguage(request);
 		String action = request.getParameter(Common.ACTION);
 		if(!StringUtils.isEmpty(action)){
-			//1 se hanno agginuto una lingua la va a salvare nel db
-			//2 aggiorna lo stato di visibilità delle lingue gestite
+			updateLanguages(request, response);
 		}
 		SqlSession sql = sqlSessionFactory.openSession();
 		//3 carico la lista delle lingue gestite
-		List<HashMap<String, Object>> list =(List<HashMap<String, Object>>)(List<?>)sql.selectList("language.list");
+		@SuppressWarnings("unchecked")
+		List<HashMap<String, Object>> managedLanguages =(List<HashMap<String, Object>>)(List<?>)sql.selectList("language.list");
+		sql.close();//non devo più fare query e quindi chiudo
+		request.setAttribute(Request.MANAGED_LANGUAGES, managedLanguages);
 		//4 carico la lista delle lingue da gestire
-		for (String locale : Locale.getISOLanguages()) {
-			//TODO
-		}
 		
+		List<Locale>toManage=new ArrayList<Locale>();
+		boolean find;
+		for (String locale : Locale.getISOLanguages()) {
+			find=false;
+			for (HashMap<String, Object> managedLanguage : managedLanguages) {
+				if(locale.equals((String)managedLanguage.get("id_language"))){
+					find=true;
+					break;
+				}
+				if(!find){
+					toManage.add(new Locale(locale));
+				}
+			}
+		}
+		request.setAttribute(Request.TO_MANAGE_LANGUAGES, toManage);
+	}
+	
+	private synchronized void updateLanguages(HttpServletRequest request,
+			HttpServletResponse response){
+		SqlSession sql = sqlSessionFactory.openSession();//sarebbe da fare in transazione
+		//1 se hanno aggiunto una lingua la va a salvare nel db
+		String id_language = request.getParameter("toManage");
+		if(!"0000".equals(id_language)){
+		HashMap<String, Object> toManage=new HashMap<String, Object>();
+		toManage.put("id_language", id_language);
+		toManage.put("is_visible", false);//abbiamo scelto che di default la visibilità è false
+		sql.insert("language.add", toManage);
+		}
+		//2 aggiorna lo stato di visibilità delle lingue gestite
+	
 	}
 	
 }
--- trunk/e-commerce/src/it/ecommerce/util/constants/Request.java
+++ trunk/e-commerce/src/it/ecommerce/util/constants/Request.java
@@ -12,4 +12,6 @@ public class Request {
 	public final static String ResourceBundle="LANG";
 	public final static String LOCALE="LOCALE";
 	public final static String managedLanguages="managedLanguages";
+	public final static String MANAGED_LANGUAGES="MANAGED_LANGUAGES";
+	public final static String TO_MANAGE_LANGUAGES="TO_MANAGE_LANGUAGES";
 }
